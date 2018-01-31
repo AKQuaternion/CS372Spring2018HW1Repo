f7c8652412cd6a5e5edd4e5081e1367461168154
Chris Hartman <chris.m.hartman@gmail.com>
2018-01-31 13:25:21 -0900

    Added main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  CS372Spring2018HW1
+//
+//  Created by Chris Hartman on 1/31/18.
+//  Copyright © 2018 Chris Hartman. All rights reserved.
+//
+
+#include <iostream>
+using std::cout;
+using std::endl;
+int main() {
+    cout << "Hello, World!\n";
+    cout << "Professor Hartman doesn't need a homework grade.\n";
+    return 0;
+}
