2e29dd0a5d33f9b2f084bcdcba8c92a2b8d933a0
Ryan Stonebraker <ryanastonebraker@gmail.com>
2018-02-06 19:31:44 -0900

    Added my grade (Ryan Stonebraker)

--- main.cpp
+++ main.cpp
@@ -9,6 +9,8 @@
 #include <iostream>
 using std::cout;
 using std::endl;
+#include <unistd.h>
+#include <string>
 
 int main() {
     cout << "Hello, World!\n";
@@ -18,14 +20,18 @@ int main() {
 	unsigned long long int plzGiveMeThisGrade = 0;
 	cout << "Hi everyone! I'm Tristan Van Cise and it would be awesome if I could get a " << --plzGiveMeThisGrade << "/20 on this assignment" << endl;
 
+  write(1, "Hi! This is Ryan Stonebraker and it would be more amazing if I could get a ", 75);
+  std::string btrGradeThnTristan = std::to_string(plzGiveMeThisGrade);
+  cout << btrGradeThnTristan.substr(0, btrGradeThnTristan.size() - 1) << plzGiveMeThisGrade % 10 + 1 << "/20 on this assignment";
+
     cout << "\nWilliam " << "Horn " << "would " << "like " << "a " << " 20 " << "/" << " 20 " << "plz." << endl;
-	
+
     cout << "Cameron K. Titus here, a 20/20 would be very nice thank you" << endl;
 
     cout << "Corey Gray: 20" << endl;
 
     cout << "Jake Conner: 20/20" << endl;
-	
+
 	cout << "Duane Shaffer: 20/20" << endl;
 
     return 0;
